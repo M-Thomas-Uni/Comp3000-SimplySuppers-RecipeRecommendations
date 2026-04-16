f1cea3f7bb822788130af78dff99d7c9a1a7b307
Morgan <morgan.thomas@students.plymouth.ac.uk>
2026-04-16 16:32:03 +0100

    Switching to all Express (I'm very inconsistent)

--- app-backend/src/server.js
+++ app-backend/src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 9000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Test frontend get</h1>');
+  res.send('Test get');
 });
 
 app.listen(port, () => {
-  console.log(`Frontend server listening on port: ${port}`);
+  console.log(`Backend server listening on port: ${port}`);
 }); 
